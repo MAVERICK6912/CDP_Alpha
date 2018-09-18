4330dbc30fd877fa97f1d2b4655bc7446b4bfbf4
Infogain <iss_gdc@hotmail.com>
2018-09-18 17:36:34 +0530

    Second commit adding course model.

--- CDP_Alpha/Models/Course.cs
+++ CDP_Alpha/Models/Course.cs
@@ -1,6 +1,14 @@
-﻿namespace CDP_Alpha.Models
+﻿using System.Collections.Generic;
+
+namespace CDP_Alpha.Models
 {
     public class Course
     {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string AuthenticationKey { get; set; }
+        public List<Feedback> Feedbacks { get; set; }
+        public int MyProperty { get; set; }
+        public List<Topic> Topics { get; set; }
     }
 }
